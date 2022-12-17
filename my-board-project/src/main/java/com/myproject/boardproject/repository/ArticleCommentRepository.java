59e62dbf837760e250c2ea68c69cd882a1664247
Kim inuk <inuk22@naver.com>
2022-12-17 23:35:35 +0900

    #1 - data rest 설정 게시글, 댓글의 json api 자동으로 rest api 생성

--- my-board-project/src/main/java/com/myproject/boardproject/repository/ArticleCommentRepository.java
+++ my-board-project/src/main/java/com/myproject/boardproject/repository/ArticleCommentRepository.java
@@ -2,6 +2,8 @@ package com.myproject.boardproject.repository;
 
 import com.myproject.boardproject.domain.ArticleComment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface ArticleCommentRepository extends JpaRepository<ArticleComment,Long> {
 }
