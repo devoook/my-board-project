b863af09ee069686d149dd9a8737a29cf7ca2ba0
Kim inuk <inuk22@naver.com>
2022-12-17 21:34:27 +0900

    #9 - jpa test 작성

--- my-board-project/src/test/java/com/myproject/boardproject/MyBoardProjectApplicationTests.java
+++ my-board-project/src/test/java/com/myproject/boardproject/MyBoardProjectApplicationTests.java
@@ -2,6 +2,7 @@ package com.myproject.boardproject;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootTest
 class MyBoardProjectApplicationTests {
