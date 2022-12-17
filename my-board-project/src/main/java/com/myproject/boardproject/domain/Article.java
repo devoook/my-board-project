67bbaf13d456841a6f817c796855e1a80e8db727
Kim inuk <inuk22@naver.com>
2022-12-17 21:31:54 +0900

    #9 - 도메인 코드를 엔티티로 변환 1 도메인 클래스 == 1 테이블 매칭되도록 설계 * 생성자와 `equasls()`, `hashcode()`등이 도메인 맥락에 맞게 디테일하게

--- my-board-project/src/main/java/com/myproject/boardproject/domain/Article.java
+++ my-board-project/src/main/java/com/myproject/boardproject/domain/Article.java
@@ -1,17 +1,92 @@
 package com.myproject.boardproject.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
 
+@Getter
+@ToString
+@Table(indexes = {
+        @Index(columnList = "title"),
+        @Index(columnList = "hashtag"),
+        @Index(columnList = "createdAt"),
+        @Index(columnList = "createdBy")
+})
+@EntityListeners(AuditingEntityListener.class)
+@Entity
 public class Article {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String title;
-    private String content;
-    private String hashtag;
 
-    private LocalDate createdAt;
+    @Setter
+    @Column(nullable = false) //@Column 기본값 true
+    private String title; // 제목
+    @Setter
+    @Column(nullable = false, length = 10000)
+    private String content; // 내용
+    @Setter
+    private String hashtag; // 해시태그
+    @ToString.Exclude // 순환참조 방지
+    @OrderBy("id") // 정렬 기준
+    @OneToMany(mappedBy = "article", cascade = CascadeType.ALL)
+    // 실무에서는 양방향 바인딩을 일부로 풀고 사용하는 경우가 많음, cascade의 속성이 필요하지 않는 경우 있음.. -> 게시글 삭제되도 댓글은 남겨야함.
+    private final Set<ArticleComment> articleComments = new LinkedHashSet<>(); // list, set , map을 다양하게 사용한다.
+
+
+    @CreatedDate
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+    @CreatedBy
+    @Column(nullable = false, length = 100)
     private String createdBy;
-    private LocalDate modifiedAt;
+    @LastModifiedDate
+    @CreatedDate
+    @Column(nullable = false)
+    private LocalDateTime modifiedAt;
+    @LastModifiedBy
+    @Column(nullable = false, length = 100)
     private String modifiedBy;
 
+    // JPA는 기본생성자 필요 , private은 안됨
+    protected Article() {
+    }
+
+    // 생성자를 private로 막고 팩토리 메서드를 통해 사용하는 방법
+    private Article(String title, String content, String hashtag) {
+        this.title = title;
+        this.content = content;
+        this.hashtag = hashtag;
+    }
+
+    // 팩토리 메스드를 사용하여 객체 생성
+    public static Article of(String title, String content, String hashtag) {
+        return new Article(title, content, hashtag);
+    }
+
+    // 동일성 동등성 검사를 위한 ... lombok의 equalsAndHashCode 사용하면 전체 필드를 사용하게됨 ..
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Article article)) return false;
+        return id != null && id.equals(article.id); // id == null 이면, 영속화가 되지 않은 것. 다른 엔티티와 연관관계를 맺을때 필요함.
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
