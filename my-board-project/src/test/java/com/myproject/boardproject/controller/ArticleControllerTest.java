554e267a4854e7e93e893f62cfb6fc8a5a07ef9c
Kim inuk <inuk22@naver.com>
2022-12-19 19:27:23 +0900

    # 15 - 게시판 뷰 기본 골격 작성 뷰 뼈대 작성 테스트에서 contentType 수정 `contentTypeCompatibleWith` 뷰 컴포넌트 분리

--- my-board-project/src/test/java/com/myproject/boardproject/controller/ArticleControllerTest.java
+++ my-board-project/src/test/java/com/myproject/boardproject/controller/ArticleControllerTest.java
@@ -22,7 +22,6 @@ class ArticleControllerTest {
         this.mvc = mvc;
     }
 
-    @Disabled("구현중")
     @DisplayName("[view][GET] 게시글 리스트 (게시판) 페이지 - 정상 호출  ")
     @Test
      void givenNothing_whenRequestingArticlesView_thenReturnsArticlesView() throws Exception {
@@ -32,7 +31,7 @@ class ArticleControllerTest {
         mvc.perform(get("/articles"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("articles/index"))
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(model().attributeExists("articles"));
     }
     @Disabled("구현중")
@@ -45,7 +44,7 @@ class ArticleControllerTest {
         mvc.perform(get("/articles/1"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("articles/detail"))
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(model().attributeExists("article"))
                 .andExpect(model().attributeExists("articleComment"));
     }
@@ -59,7 +58,7 @@ class ArticleControllerTest {
         mvc.perform(get("/articles/search"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("articles/search"))
-                .andExpect(content().contentType(MediaType.TEXT_HTML));
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML));
     }
     @Disabled("구현중")
     @DisplayName("[view][GET] 게시글 해시태그 검색 전용 페이지 - 정상 호출  ")
@@ -71,6 +70,6 @@ class ArticleControllerTest {
         mvc.perform(get("/articles/search-hashtag"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("articles/search-hashtag"))
-                .andExpect(content().contentType(MediaType.TEXT_HTML));
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML));
     }
 }
