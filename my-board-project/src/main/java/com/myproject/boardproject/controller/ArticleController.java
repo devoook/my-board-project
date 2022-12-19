24b5647825d5bbf5ce6a7215f5d69c45094bd6e6
Kim inuk <inuk22@naver.com>
2022-12-19 18:41:19 +0900

    #15 - 게시글 관련 뷰 테스트 작성

---
+++ my-board-project/src/main/java/com/myproject/boardproject/controller/ArticleController.java
@@ -0,0 +1,12 @@
+package com.myproject.boardproject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/articles")
+@Controller
+public class ArticleController {
+
+
+
+}
