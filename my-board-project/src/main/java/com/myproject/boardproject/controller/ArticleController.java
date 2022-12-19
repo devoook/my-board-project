256b8949603ebf88433b15d12b0adbb0f6dddd6d
Kim inuk <inuk22@naver.com>
2022-12-19 22:23:24 +0900

    #15 - 로그인 컨트롤러 & 테스트
    스프링 시큐리티가 자동 생성해준 `/login` 컨트롤러 테스트. 모든 컨트롤러 설정을 위한 `@Import`추가

--- my-board-project/src/main/java/com/myproject/boardproject/controller/ArticleController.java
+++ my-board-project/src/main/java/com/myproject/boardproject/controller/ArticleController.java
@@ -20,7 +20,7 @@ public class ArticleController {
 
     @GetMapping("/{articleId}")
     public String articles(@PathVariable Long articleId, ModelMap map) {
-        map.addAttribute("article", "article"); //TODO: article value 실제 데이터 넣을 것. 
+        map.addAttribute("article", "article"); //TODO: article value 실제 데이터 넣을 것.
         map.addAttribute("articleComment", List.of());
         return "articles/detail";
     }
