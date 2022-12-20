37dd60ba24b7f873044098bf2e2e33ebb4e33add
Kim inuk <inuk22@naver.com>
2022-12-20 14:41:30 +0900

    UserAccount 코드 추가
    빠른 추가를 위해 바로 main에 push

--- my-board-project/src/test/java/com/myproject/boardproject/controller/DataRestTest.java
+++ my-board-project/src/test/java/com/myproject/boardproject/controller/DataRestTest.java
@@ -10,9 +10,10 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 
 @Disabled("Spring Data REST 통합테스트는 불필요하므로 제외")
 @DisplayName("Data Rest - API 테스트")
@@ -29,7 +30,7 @@ public class DataRestTest {
 
     @DisplayName("[API] 게시글 리스트 조회")
     @Test
-     void givenNothing_whenRequestingArticles_thenReturnArticlesJsonResponse() throws Exception {
+    void givenNothing_whenRequestingArticles_thenReturnArticlesJsonResponse() throws Exception {
         //given
 
         //when & then
@@ -80,6 +81,20 @@ public class DataRestTest {
         mvc.perform(get("/api/articleComments/1"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.valueOf("application/hal+json")));
+    }
+
+    @DisplayName("[API] 회원 관련 API는 제공하지 않음  ")
+    @Test
+    public void givenNothing_whenRequestingUserAccounts_thenThrowsException() throws Exception {
+        //given
+
+        //when&then
+        mvc.perform(get("/api/userAccounts")).andExpect(status().isNotFound());
+        mvc.perform(post("/api/userAccounts")).andExpect(status().isNotFound());
+        mvc.perform(put("/api/userAccounts")).andExpect(status().isNotFound());
+        mvc.perform(patch("/api/userAccounts")).andExpect(status().isNotFound());
+        mvc.perform(delete("/api/userAccounts")).andExpect(status().isNotFound());
+        mvc.perform(head("/api/userAccounts")).andExpect(status().isNotFound());
     }
 
 }
